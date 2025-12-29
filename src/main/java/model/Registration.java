10e76a0b72ac78bcb5e9b694db86f3d09976d137
Mohamed Asad <mohamedasad11914@gmail.com>
2025-12-29 15:10:57 +0200

    Add Registration entity with JPA annotations and relationships

---
+++ src/main/java/model/Registration.java
@@ -0,0 +1,30 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "registrations")
+public class Registration {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+    
+    @ManyToOne
+    @JoinColumn(name = "module_id", nullable = false)
+    private Module module;
+    
+    private Double mark;
+    
+    private String semester;
+}
