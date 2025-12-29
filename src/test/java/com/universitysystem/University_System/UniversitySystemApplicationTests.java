b56267f1dae80c848011275fd8bd4b07e1f6433e
Mohamed Asad <mohamedasad11914@gmail.com>
2025-12-29 11:49:21 +0200

    Initial commit - Spring Boot University System

---
+++ src/test/java/com/universitysystem/University_System/UniversitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universitysystem.University_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
